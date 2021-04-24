2afa1485b21d59f6e9a6041e76b2d59510494b17
Devin Duval <devinduval09@gmail.com>
2021-04-23 21:26:31 -0700

    room now tracks and displays exits and who is in the room

--- room.py
+++ room.py
@@ -1,22 +1,27 @@
 from items import item_dict
 
 class Room(object):
-    def __init__(self, number, description, inventory=[], exits={}):
+    def __init__(self, number, description, inventory=[], characters=[], exits={}):
         self.number = number
         self._description = description
         self.inventory = inventory
         self.exits = exits
+        self.characters = characters
     
     def description(self):
         '''Provide description of room along with any a list of interactable items'''
-        print(self.inventory)
+        desc = self._description
         if self.inventory:
-            return self._description + f' In the room you see a {" ".join([item_dict[item].description() for item in self.inventory])}.'
-        else:
-            return self._description
+            desc += f'\nIn the room you see a {" ".join([item_dict[item].description() for item in self.inventory])}.'
+        if self.characters:
+            desc += f'\nStanding in the room you see {self.characters}.'
+        
+        if self.exits.keys():
+            desc += f'\n There are exits to the {self.exits.keys()}.'
+        return desc
 
 
 rooms_dict = {0: Room(0, 'A room with doors headed north, west, and east.', inventory=['magic box'], exits={'north': 3, 'west': 1, 'east': 2}),
               1: Room(1, 'A room with a door headed east.', inventory=['steel sword'], exits={'east': 0}),
               2: Room(2, 'A room with a door headed west.', inventory=['wooden shield', 'chainmail shirt'], exits={'west': 0}),
-              3: Room(3, 'A room with a door headed south.', exits={'south': 0})}
+              3: Room(3, 'A room with a door headed south.', characters=['training dummy'], exits={'south': 0})}
