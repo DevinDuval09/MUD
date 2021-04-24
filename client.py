83ecbaea8d604a6cb529b31f865533c16fdf6f48
Devin Duval <devinduval09@gmail.com>
2021-04-23 21:24:50 -0700

    client now async to constantly receive pushes from server

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
 import socket
 import sys
+import asyncio
 from os import system
-
+#TODO: make client async
 #system('python -m serve')
 
 try:
@@ -16,15 +17,27 @@ except IndexError:
 
 client_socket = socket.socket()
 client_socket.connect((host, port))
-
-while True:
-    try:
-        response = client_socket.recv(4096).decode()
-    except ConnectionAbortedError:
-        print("Connection closed by host.")
-        break
-
-    print(response)
-
+async def send_command():
     my_message = input("> ").encode('utf-8') + b'\n'
     client_socket.sendall(my_message)
+    await asyncio.sleep(.25)
+
+async def receive_response():
+    response = client_socket.recv(4096).decode()
+    print(response)
+    await asyncio.sleep(.25)
+
+async def main():
+    while True:
+        try:
+            server_response = asyncio.create_task(receive_response())
+            client_input = asyncio.create_task(send_command())
+            await server_response
+            await client_input
+        except ConnectionAbortedError:
+            print("Connection closed by host.")
+            break
+
+asyncio.run(main())
+
+
