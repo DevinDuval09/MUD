8c124a3d9852c296fffefc1aba8e486ba13da313
Devin Duval <devinduval09@gmail.com>
2021-04-23 21:27:34 -0700

    added bash. not useful

--- skills.py
+++ skills.py
@@ -1,4 +1,9 @@
-'''Injectable skills that a character can learn / obtain'''
+'''Injectable skills that a character can learn / obtain.
+   These are all active skills.'''
 
 def block(self):
     return "You wave your shield around to block huge sweeping bats."
+
+def bash(self):
+    return "You bash your opponent in the face with the flat of your blade."
+
