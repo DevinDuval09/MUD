673f1370b303bcfe02a0c88f4f9f450b65b7853f
Devin Duval <devinduval09@gmail.com>
2021-04-25 11:40:35 -0700

    added to_hit_roll function

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 from character import Character
 from room import rooms_dict
-from items import item_dict
+from items import item_dict, Equipment
 from random import randint
 #TODO: player inputs during combat
 
@@ -12,6 +12,10 @@ def roll_d20():
 def roll_d10():
     '''generate random number between 1 and 10'''
     return randint(1, 10)
+
+def to_hit_roll(player:Character, weapon:Equipment)->int:
+    bonus = player.proficiency_skills.get(weapon.associated_skill, 0)
+    return roll_d20() + bonus
 
 #TODO: add server logging
 class Server(object):
