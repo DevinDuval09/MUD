a678dc4b45f9fcaf151e0f72ccece012eda32ae1
Devin Duval <devinduval09@gmail.com>
2021-04-25 14:23:36 -0700

    deleted skills.py

--- skills.py
+++
@@ -1,6 +0,0 @@
-'''Injectable skills that a character can learn / obtain.
-   These are all active skills.'''
-from character import Character
-from global_funcs import roll_d10, roll_d20, to_hit_roll
-from items import Equipment
-from global_collections import item_dict
