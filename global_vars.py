e3e1657387dccbf8552b4bb5a4725a8beaec524f
Devin Duval <devinduval09@gmail.com>
2021-04-26 10:00:30 -0700

    added training dummy to global vars

--- global_vars.py
+++ global_vars.py
@@ -1,6 +1,7 @@
 from items import Item, Container, Equipment
 from room import Room
 from skills import block, bash
+from character import Character 
 
 item_dict = {'magic box': Container(0, 'magic box', inventory_items=[], strength=2),
              'magical crystal': Item(1, 'magical crystal', strength=5),
@@ -11,7 +12,11 @@ item_dict = {'magic box': Container(0, 'magic box', inventory_items=[], strength
 
 item_dict['magic box'].inventory.append(item_dict['magical crystal'])
 
+training_dummy = Character('training dummy', 3)
+training_dummy.inventory.append(item_dict['book of butt kicking'])
 rooms_dict = {0: Room(0, 'A room with doors headed north, west, and east.', inventory=[item_dict['magic box']], exits={'north': 3, 'west': 1, 'east': 2}),
               1: Room(1, 'A room with a door headed east.', inventory=[item_dict['steel sword']], exits={'east': 0}),
               2: Room(2, 'A room with a door headed west.', inventory=[item_dict['wooden shield'], item_dict['chainmail shirt']], exits={'west': 0}),
-              3: Room(3, 'A room with a door headed south.', characters=['training dummy'], exits={'south': 0})}
+              3: Room(3, 'A room with a door headed south.', characters=[training_dummy], exits={'south': 0})}
+
+training_dummy.room = rooms_dict[3]
