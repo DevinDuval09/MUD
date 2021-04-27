25df3d026624f165e46a529b5fe05e3d6e3d1581
Devin Duval <devinduval09@gmail.com>
2021-04-27 08:45:14 -0700

    added roll_d4

--- utilities.py
+++ utilities.py
@@ -11,6 +11,9 @@ def roll_d10():
     '''generate random number between 1 and 10'''
     return randint(1, 10)
 
+def roll_d4():
+    return randint(1, 4)
+
 def to_hit_roll(player:Character, weapon:Equipment)->int:
     bonus = player.proficiency_skills.get(weapon.associated_skill, 0)
     return roll_d20() + bonus
