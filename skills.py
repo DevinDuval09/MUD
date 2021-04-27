3f6d1480a7eb12ddc7ae65e9bb5534135c13cff4
Devin Duval <devinduval09@gmail.com>
2021-04-27 08:45:43 -0700

    bash now working

--- skills.py
+++ skills.py
@@ -2,13 +2,15 @@ from character import Character
 from items import Equipment
 from utilities import to_hit_roll, roll_d10, roll_d20
 
-def bash(self, target:Character, weapon:Equipment=None)->str:
+def bash(self, target:Character)->str:
+    print('self: ', self.name)
+    print('target: ', target.name)
     if self.equipment['off hand']:
         weapon = self.equipment['off hand']
     else:
         weapon = Equipment(None, 'fist',slot='off hand', associated_skill=None)
     to_hit = to_hit_roll(self, weapon)
-    if to_hit >= target.__get_stat('armor'):
+    if to_hit >= target._Character__get_stat('armor'):
         if weapon._description == 'fist':
             damage = max(roll_d10() - 5, 1)
         else:
